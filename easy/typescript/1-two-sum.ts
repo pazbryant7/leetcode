e3593ddf638a35ef3f5854247323937bf8e27f4b
Bryant Paz <pazbryant7@outlook.com>
2025-08-19 21:10:17 -0600

    feat(1-two-sum): update answer, adding one more

--- easy/typescript/1-two-sum.ts
+++ easy/typescript/1-two-sum.ts
@@ -1,19 +1,3 @@
-function twoSumBruteForce(nums: number[], target: number): number[] {
-  let twoSumIndex: number[] = [];
-
-  // HACK: Stop outer loop from inner loop
-  outerLoop: for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        twoSumIndex.push(i, j);
-        break outerLoop;
-      }
-    }
-  }
-
-  return twoSumIndex;
-}
-
 // HACK: One pass / Hash map
 function twoSum(nums: number[], target: number): number[] {
   const cachedNumbers: Map<number, number> = new Map();
@@ -29,4 +13,34 @@ function twoSum(nums: number[], target: number): number[] {
   }
 
   return [];
+}
+
+function twoSum1(nums: number[], target: number): number[] {
+  const seen: { [num: string]: number } = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const x = target - nums[i];
+    if (seen[x] != null) {
+      return [i, seen[x]];
+    }
+    seen[nums[i]] = i;
+  }
+
+  return [];
+}
+
+function twoSum2(nums: number[], target: number): number[] {
+  let twoSumIndex: number[] = [];
+
+  // HACK: Stop outer loop from inner loop
+  outerLoop: for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        twoSumIndex.push(i, j);
+        break outerLoop;
+      }
+    }
+  }
+
+  return twoSumIndex;
 }
